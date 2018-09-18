9f9385b9273ec7554845de13ec4815b15dced8bb
jacky810124 <jacky810124@hotmail.com>
2018-09-18 12:12:26 +0800

    ✨ Feat: sync subscription data to server side

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const PUBLIC_KEY = 'BCDMOxYAhVUZY1cXwkXsKuKztlqfOXjowcriucykb5qBmFq-8lVMVx3bJbFOmLSci1Jq_SPqBdFeWGi0jcHJfXM'
 const NOTIFICATION_USABILITY = 'serviceWorker' in navigator && 'PushManager' in window
+const HAS_FINGERPRINT = !(localStorage.getItem('fingerprint') == null && localStorage.getItem('fingerprint') === '')
+const API_HOST = ''
 
 /**
  * initialization function
  */
 const init = () => {
   const subscribeButton = document.querySelector('.user-profile__subscribe-button')
+
+  if (HAS_FINGERPRINT) {
+    generateFingerprint()
+      .then(fingerprint => {
+        localStorage.setItem('fingerprint', fingerprint)
+      })
+  }
 
   if (!NOTIFICATION_USABILITY) {
     console.log('Service Worker and Push is supported')
@@ -43,7 +52,19 @@ const getSubscription = (registration) => {
     .getSubscription()
 }
 const saveSubscription = (subscription) => {
-  // TODO: upload subscription data to server side
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify({
+      fingerprint: localStorage.getItem('fingerprint'),
+      subscription
+    }),
+    headers: new Headers({
+      'Content-Type': 'Application/json'
+    })
+  }
+
+  return fetch(`${API_HOST}/subscriptions`, options)
+    .then(response => response.json())
 }
 const requestSubscription = (registration) => {
   const result = confirm('是否要訂閱 Jacky ？')
